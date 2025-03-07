69d76783106e1ec5f7eb2637a8f072548b56c479
vizdan123 <velievvizdan2100@gmail.com>
2025-03-07 22:48:13 +0700

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -119,33 +119,9 @@ createButton("ESP", UDim2.new(0, 5, 0.24, 0), function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/thaibao/refs/heads/main/Show%20esp.lua"))()
     end)
 end)
-
 createButton("ctrl+click tp", UDim2.new(0, 5, 0.31, 0), function()
     print("ctrl+click tp  активирован")
-    -- --[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-local UIS = game:GetService("UserInputService")
-
-local Player = game.Players.LocalPlayer
-local Mouse = Player:GetMouse()
-
-
-function GetCharacter()
-   return game.Players.LocalPlayer.Character
-end
-
-function Teleport(pos)
-   local Char = GetCharacter()
-   if Char then
-       Char:MoveTo(pos)
-   end
-end
-
-
-UIS.InputBegan:Connect(function(input)
-   if input.UserInputType == Enum.UserInputType.MouseButton1 and UIS:IsKeyDown(Enum.KeyCode.LeftControl) then
-       Teleport(Mouse.Hit.p)
-   end
-end)
+     pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vizdan123/asd/refs/heads/main/asd.lua"))()
+    end)
 end)
